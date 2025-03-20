fd96791d026a24c93348e7e098c281d5450d8d79
lsalatiel <lucassalatiel1004@gmail.com>
2025-03-20 17:16:55 -0300

    config db

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ import Config
 # Configure your database
 config :salatwitter, Salatwitter.Repo,
   username: "postgres",
-  password: "postgres",
+  # password: "postgres",
   hostname: "localhost",
-  database: "salatwitter_dev",
+  database: "salatwitter_db",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
